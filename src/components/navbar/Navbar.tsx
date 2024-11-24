19e965ff51b1fcfdf08343c62fc05090d42d53e6
Alamin11 <alamincsecu@gmail.com>
2024-11-24 17:28:39 +0600

    added all settings

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { url } from "inspector";
+import Link from "next/link";
+import { title } from "process";
+import React from "react";
+import styles from "./page.module.css";
+
+const Navbar = () => {
+  const navItems = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <Link href={"/"} className={styles.logo}>
+        DayDreamers
+      </Link>
+      <div className={styles.links}>
+        {navItems.map((item) => (
+          <Link key={item.id} href={item.url} className={styles.link}>
+            {item.title}
+          </Link>
+        ))}
+        <button
+          onClick={() => {
+            console.log("Logged out");
+          }}
+          className={styles.logout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
